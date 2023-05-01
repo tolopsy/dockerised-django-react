cf8d33923fdff30d2d2ea18b7efebf63ec8881b2
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-05-01 17:24:58 +0100

    use docker compose

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for core project.
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
 ]
