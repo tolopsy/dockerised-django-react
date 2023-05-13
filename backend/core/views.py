e1b56e38a74b7d24122dc660a198a55f987744a2
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-05-13 14:42:53 +0100

    use default base url page

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
-def test_view(request):
-    return HttpResponse("working")
-
 def json_test_view(request):
     return JsonResponse({"active": True})
